5dbbec8f04f41f6f4f449198a5f084c08e36255b
Benjamin Gudehus <hastebrot@gmail.com>
2016-02-13 20:42:12 +0100

    (feat) Add MainController and use it in Main view.

--- projects/vaucansonfx/src/main/kotlin/hastebrot/vaucansonfx/main.kt
+++ projects/vaucansonfx/src/main/kotlin/hastebrot/vaucansonfx/main.kt
@@ -2,20 +2,36 @@ package hastebrot.vaucansonfx
 
 import javafx.application.Application
 import javafx.application.Platform
+import javafx.scene.input.KeyCode
 import javafx.scene.layout.StackPane
 import javafx.stage.Screen
+import javafx.stage.Stage
 import javafx.stage.Window
 import tornadofx.App
+import tornadofx.Controller
 import tornadofx.View
 import tornadofx.label
 
 class Main : View() {
     override val root = StackPane()
 
+    private val controller by inject<MainController>()
+
     init {
-        root.setPrefSize(500.0, 500.0)
-        root.label("Hello World!")
+        with(root) {
+            setPrefSize(500.0, 500.0)
+            label("Hello World")
+        }
+
+        println(controller.foo())
+
         Platform.runLater {
+            root.scene.setOnKeyReleased {
+                when (it.code) {
+                    KeyCode.ESCAPE -> (root.scene.window as Stage).close()
+                    else -> Unit
+                }
+            }
             centerOnHalfScreen(root.scene.window)
         }
     }
@@ -25,6 +41,10 @@ class Main : View() {
         val stageLeft = (halfScreen - window.width) / 2.0
         window.x = stageLeft
     }
+}
+
+class MainController : Controller() {
+    fun foo() = "foo"
 }
 
 class MainApp : App() {
